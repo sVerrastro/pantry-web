980d20ba12ea09011946dad9d957b7f9fc38cef1
Sergio Verrastro <sergio.09.2005@gmail.com>
2026-03-05 15:24:44 +0100

    Fix API URL and CSS import

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://pantry-backend-8aac.onrender.com';
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
